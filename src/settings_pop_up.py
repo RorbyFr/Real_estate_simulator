cbc210afbeab97dd314a274500166fa667b0d574
RorbyFr <201471039+RorbyFr@users.noreply.github.com>
2025-03-22 12:58:16 +0100

    RES-7: Identify current language and money on GUI

--- src/settings_pop_up.py
+++ src/settings_pop_up.py
@@ -23,29 +23,63 @@ class SettingsPopUp(Ui_Dialog, common.RoundedQdialog):
         # Remove upside bar and keep pop-up and first view
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
 
+        # Button info keys
+        self.BUTTON_INSTANCE = "button instance"
+        self.BUTTON_FILE = "button file"
+        self.BUTTON_PICTURE = "button picture"
+        self.BUTTON_SYMBOL = "button symbol"
+
+        # Language list of dictionary containing info buttons
+        self.language_button_list = [
+            {self.BUTTON_INSTANCE: self.english_translation_pushButton,
+             self.BUTTON_FILE: "en.qm",
+             self.BUTTON_PICTURE: "english.png"
+             },
+            {self.BUTTON_INSTANCE: self.french_translation_pushButton,
+             self.BUTTON_FILE: "fr.qm",
+             self.BUTTON_PICTURE: "french.png"
+             },
+        ]
+        # Money list of dictionary containing info buttons
+        self.money_button_list = [
+            {self.BUTTON_INSTANCE: self.euro_pushButton,
+             self.BUTTON_SYMBOL: "€",
+             self.BUTTON_PICTURE: "euro_money.png"
+             },
+            {self.BUTTON_INSTANCE: self.pound_pushButton,
+             self.BUTTON_SYMBOL: "£",
+             self.BUTTON_PICTURE: "pound_money.png"
+             },
+            {self.BUTTON_INSTANCE: self.dollar_pushButton,
+             self.BUTTON_SYMBOL: "$",
+             self.BUTTON_PICTURE: "dollar_money.png"
+             },
+            {self.BUTTON_INSTANCE: self.yen_pushButton,
+             self.BUTTON_SYMBOL: "¥",
+             self.BUTTON_PICTURE: "yen_money.png"
+             },
+            {self.BUTTON_INSTANCE: self.rupee_pushButton,
+             self.BUTTON_SYMBOL: "₹",
+             self.BUTTON_PICTURE: "rupee_money.png"
+             },
+            {self.BUTTON_INSTANCE: self.ruble_pushButton,
+             self.BUTTON_SYMBOL: "₽",
+             self.BUTTON_PICTURE: "ruble_money.png"
+             },
+        ]
+
         # Load translation image
-        common.load_scaled_icon_on_widget(self.english_translation_pushButton,
-                                          os.path.join(common.RESOURCES_PATH, "english.png"),
-                                          text_under=False, border_width=4, border_height=4)
-        common.load_scaled_icon_on_widget(self.french_translation_pushButton,
-                                          os.path.join(common.RESOURCES_PATH, "french.png"),
-                                          text_under=False, border_width=4, border_height=4)
+        for dict_language in self.language_button_list:
+            common.load_scaled_icon_on_widget(dict_language[self.BUTTON_INSTANCE],
+                                              os.path.join(common.RESOURCES_PATH, dict_language[self.BUTTON_PICTURE]),
+                                              text_under=False, border_width=4, border_height=4)
 
         # Load money image
         margin_border = 10
-        common.load_scaled_icon_on_widget(self.euro_pushButton, os.path.join(common.RESOURCES_PATH, "euro_money.png"),
-                                          text_under=False, border_width=margin_border, border_height=margin_border)
-        common.load_scaled_icon_on_widget(self.pound_pushButton, os.path.join(common.RESOURCES_PATH, "pound_money.png"),
-                                          text_under=False, border_width=margin_border, border_height=margin_border)
-        common.load_scaled_icon_on_widget(self.dollar_pushButton,
-                                          os.path.join(common.RESOURCES_PATH, "dollar_money.png"),
-                                          text_under=False, border_width=margin_border, border_height=margin_border)
-        common.load_scaled_icon_on_widget(self.yen_pushButton, os.path.join(common.RESOURCES_PATH, "yen_money.png"),
-                                          text_under=False, border_width=margin_border, border_height=margin_border)
-        common.load_scaled_icon_on_widget(self.rupee_pushButton, os.path.join(common.RESOURCES_PATH, "rupee_money.png"),
-                                          text_under=False, border_width=margin_border, border_height=margin_border)
-        common.load_scaled_icon_on_widget(self.ruble_pushButton, os.path.join(common.RESOURCES_PATH, "ruble_money.png"),
-                                          text_under=False, border_width=margin_border, border_height=margin_border)
+        for dict_money in self.money_button_list:
+            common.load_scaled_icon_on_widget(dict_money[self.BUTTON_INSTANCE],
+                                              os.path.join(common.RESOURCES_PATH, dict_money[self.BUTTON_PICTURE]),
+                                              text_under=False, border_width=margin_border, border_height=margin_border)
 
         # Apply specific style on button
         self.apply_style_button()
@@ -53,52 +87,90 @@ class SettingsPopUp(Ui_Dialog, common.RoundedQdialog):
         # Connect button
         self.connect_all_button()
 
-        # Initial french language
+        # Initial French language
         self.current_language = None
 
+        # Initial french button clicked
+        self.last_language_button = self.french_translation_pushButton
+
+        # Initial euro button clicked
+        self.last_money_button = self.euro_pushButton
+
     def apply_style_button(self):
-        # Flat button
-        self.english_translation_pushButton.setFlat(True)
-        self.french_translation_pushButton.setFlat(True)
-        self.euro_pushButton.setFlat(True)
-        self.pound_pushButton.setFlat(True)
-        self.dollar_pushButton.setFlat(True)
-        self.yen_pushButton.setFlat(True)
-        self.rupee_pushButton.setFlat(True)
-        self.ruble_pushButton.setFlat(True)
-        # Pointer style
-        self.english_translation_pushButton.setCursor(Qt.PointingHandCursor)
-        self.french_translation_pushButton.setCursor(Qt.PointingHandCursor)
-        self.euro_pushButton.setCursor(Qt.PointingHandCursor)
-        self.pound_pushButton.setCursor(Qt.PointingHandCursor)
-        self.dollar_pushButton.setCursor(Qt.PointingHandCursor)
-        self.yen_pushButton.setCursor(Qt.PointingHandCursor)
-        self.rupee_pushButton.setCursor(Qt.PointingHandCursor)
-        self.ruble_pushButton.setCursor(Qt.PointingHandCursor)
+        """Use flat style and PointingHandCursor"""
+        for dict_language in self.language_button_list:
+            dict_language[self.BUTTON_INSTANCE].setFlat(True)
+            dict_language[self.BUTTON_INSTANCE].setCursor(Qt.PointingHandCursor)
+        for dict_money in self.money_button_list:
+            dict_money[self.BUTTON_INSTANCE].setFlat(True)
+            dict_money[self.BUTTON_INSTANCE].setCursor(Qt.PointingHandCursor)
 
     def connect_all_button(self):
         # Connect translation pushButton
-        self.english_translation_pushButton.clicked.connect(lambda translate, qm="en.qm": self.launch_translate(qm))
-        self.french_translation_pushButton.clicked.connect(lambda translate, qm="fr.qm": self.launch_translate(qm))
+        for dict_language in self.language_button_list:
+            dict_language[self.BUTTON_INSTANCE].clicked.connect(lambda translate, qm=dict_language[self.BUTTON_FILE]:
+                                                                self.launch_translate(qm))
 
         # Connect money pushButton
-        self.euro_pushButton.clicked.connect(lambda change_money, money="€": self.change_money(money))
-        self.pound_pushButton.clicked.connect(lambda change_money, money="£": self.change_money(money))
-        self.dollar_pushButton.clicked.connect(lambda change_money, money="$": self.change_money(money))
-        self.yen_pushButton.clicked.connect(lambda change_money, money="¥": self.change_money(money))
-        self.rupee_pushButton.clicked.connect(lambda change_money, money="₹": self.change_money(money))
-        self.ruble_pushButton.clicked.connect(lambda change_money, money="₽": self.change_money(money))
+        for dict_money in self.money_button_list:
+            dict_money[self.BUTTON_INSTANCE].clicked.connect(lambda change_money, money=dict_money[self.BUTTON_SYMBOL]:
+                                                             self.change_money(money))
 
         # Connect pop up closure to ok button
         self.ok_pushButton.clicked.connect(self.close_pop_up)
 
+    def get_button_from_element(self, key, key_value):
+        """ Get the button instance from key
+
+        :params key: string key in (self.BUTTON_FILE, self.BUTTON_PICTURE, self.BUTTON_SYMBOL)
+        :params key_value: string value corresponding to key
+        :return: QPushButton instance button
+        """
+
+        for button_list in [self.language_button_list, self.money_button_list]:
+            # Verify if key exist in list of dict
+            if key not in button_list[0].keys():
+                continue
+            # Search for dict with tuple (key, key_value)
+            for target_dict in button_list:
+                if (key, key_value) in target_dict.items():
+                    return target_dict[self.BUTTON_INSTANCE]
+
+    def update_clicked_button(self, button, language_button):
+        """Non-enable clicked button and enable last button clicked
+
+        :params button: QPushButton button
+        :params language_button: boolean to update language or money button
+        :return: None
+        """
+        # Enabled old button
+        last_button = self.last_language_button if language_button else self.last_money_button
+        last_button.setEnabled(True)
+
+        # Non-enable clicked button
+        button.setEnabled(False)
+
+        # Update last button clicked
+        if language_button:
+            self.last_language_button = button
+        else:
+            self.last_money_button = button
+
     @Slot(str)
     def change_money(self, money):
+        # Update current money button, don't use sender() because change_money can be call without button clicking
+        button = self.get_button_from_element(self.BUTTON_SYMBOL, money)
+        self.update_clicked_button(button, False)
+        # Change global money variable and retranslate
         common.MONEY_UNIT = money
         self.launch_translate(self.current_language)
 
     @Slot(str)
     def launch_translate(self, text):
+        # Update current language button, don't use sender() because change_money can be call without button clicking
+        button = self.get_button_from_element(self.BUTTON_FILE, text)
+        self.update_clicked_button(button, True)
+        # Retranslate text
         self.current_language = text
         self.sig_translate.emit(text)
 
